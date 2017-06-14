b8b0377bff957918b05570b3bf2b1ea917f76652
Kuba <kubawozniak27@gmail.com>
2017-06-14 20:47:26 +0200

    First Commit

---
+++ ChatWithSignalR/Hubs/CounterOnlineUser.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Microsoft.AspNet.SignalR;
+using System.Threading.Tasks;
+
+namespace ChatWithSignalR.Hubs
+{
+    public class CounterOnlineUser : Hub
+    {
+        private static int counter = 0;
+
+        public void Send()
+        {
+            string message = "";
+            
+            if (counter < 2)
+                message = string.Format("Obecnie jest {0} użytkownik online", counter);
+            else
+                message = string.Format("Obecnie jest {0} użytkowników online.", counter);
+
+            Clients.All.recalculateOnlineUsers(message);
+        }
+
+
+
+        
+        public override Task OnConnected()
+        {
+            counter++;
+            return base.OnConnected();
+        }
+
+        public override Task OnDisconnected(bool stopCalled)
+        {
+            counter--;
+            return base.OnDisconnected(stopCalled);
+        }
+    }
+}
---
+++ ChatWithSignalR/Models/MessageDetails.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ChatWithSignalR.Models
+{
+    public class MessageDetails
+    {
+        public string UserName { get; set; }
+
+        public string Message { get; set; }
+    }
+}
